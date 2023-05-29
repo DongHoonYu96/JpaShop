6445df6e213f7a6d2fea04a21876fedc13c187c4
dbehdgns71@naver.com <dbehdgns71@naver.com>
2023-05-30 01:11:04 +0900

    jpasho init

--- jpashop/src/main/java/jpabook/jpashop/Hello.java
+++ jpashop/src/main/java/jpabook/jpashop/Hello.java
@@ -1,2 +1,10 @@
-package jpabook.jpashop;public class Hello {
+package jpabook.jpashop;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter //hello.getData(return data) 자동생성
+@Setter //hello.setData(String data) 자동생성
+public class Hello {
+    private String data;
 }

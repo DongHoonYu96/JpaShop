b56f31e596ba7ae8928b3d282ed86f6ccfeff52c
dbehdgns71@naver.com <dbehdgns71@naver.com>
2023-08-20 23:32:22 +0900

    0820 게시판 완성

--- board/src/main/java/com/study/board/controller/BoardController.java
+++ board/src/main/java/com/study/board/controller/BoardController.java
@@ -3,6 +3,10 @@ package com.study.board.controller;
 import com.study.board.entity.Board;
 import com.study.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +20,35 @@ public class BoardController {
 
     @Autowired
     private BoardService boardService;
+
+    @GetMapping("/")
+    public String home(Model model,                //결과들을 pageList화 하고 정렬시켜준다.
+                       @PageableDefault(page = 0,size=10,sort="id",direction = Sort.Direction.DESC) Pageable pageable,
+                       String searchKeyword){
+
+        Page<Board> list=null;
+
+        //전체검색
+        list=boardService.boardList(pageable);
+
+
+
+        //페이지 선택 번호칸이 10개씩 나오도록 구현 ( 1 2 3 4 5 6 7 8 9 10)
+        int nowPage=list.getPageable().getPageNumber()+1;   //0번부터시작이므로
+        int startPage=Math.max(nowPage-4,1);
+        int endPage=Math.min(nowPage+5,list.getTotalPages());
+
+        model.addAttribute("list",list);
+        model.addAttribute("nowPage",nowPage);
+        model.addAttribute("startPage",startPage);
+        model.addAttribute("endPage",endPage);
+
+        //1. boardList메소드에 리턴된값을 model에담아서
+        //2.list라는 이름으로 html에 넘긴다
+        //model.addAttribute("list",boardService.boardList(pageable));
+
+        return "boardlist.html";
+    }
 
     @GetMapping("/board/write") //lcalhost:8080/board/write 에 접속하면 실행
     public String boardWritingForm(){
@@ -38,11 +71,35 @@ public class BoardController {
     }
 
     @GetMapping("/board/list")
-    public String boardList(Model model){
+    public String boardList(Model model,                //결과들을 pageList화 하고 정렬시켜준다.
+                            @PageableDefault(page = 0,size=10,sort="id",direction = Sort.Direction.DESC) Pageable pageable,
+                            String searchKeyword){
+
+        Page<Board> list=null;
+
+        //전체검색
+        if(searchKeyword==null){
+             list=boardService.boardList(pageable);
+        }
+        else{  // 키워드검색
+            list=boardService.boardSearchList(searchKeyword,pageable);
+        }
+
+
+
+        //페이지 선택 번호칸이 10개씩 나오도록 구현 ( 1 2 3 4 5 6 7 8 9 10)
+        int nowPage=list.getPageable().getPageNumber()+1;   //0번부터시작이므로
+        int startPage=Math.max(nowPage-4,1);
+        int endPage=Math.min(nowPage+5,list.getTotalPages());
+
+        model.addAttribute("list",list);
+        model.addAttribute("nowPage",nowPage);
+        model.addAttribute("startPage",startPage);
+        model.addAttribute("endPage",endPage);
 
         //1. boardList메소드에 리턴된값을 model에담아서
         //2.list라는 이름으로 html에 넘긴다
-        model.addAttribute("list",boardService.boardList());
+        //model.addAttribute("list",boardService.boardList(pageable));
 
         return "boardlist.html";
     }

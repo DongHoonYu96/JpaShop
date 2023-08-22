c89c47e5f5d3cc0567c50aa4bc372be8bc4dcee7
dbehdgns71@naver.com <dbehdgns71@naver.com>
2023-08-22 23:30:00 +0900

    230822 desktop

---
+++ exam/src/main/reactfront/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  let name = "Bisu";
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>{name}</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;

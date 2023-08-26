edb0570f7f23f29f8bd78dd6eb0c2195651ee0c2
dbehdgns71@naver.com <dbehdgns71@naver.com>
2023-08-26 18:10:22 +0900

    230826 desk

--- exam/src/main/reactfront/src/App.js
+++ exam/src/main/reactfront/src/App.js
@@ -1,17 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [text, setText] = useState("123");
+  
+  const [edit, setEdit] = useState(true);
 
-  let name = "Bisu";
-
+  if (edit) {
+    return(
+      <div>
+      <input type="text" value={text}
+        //이벤트가 발생되면 (텍스트갱신) => 자동 랜더링됨
+        //text는 event.target.value에 잇음
+        onChange={(e) => {
+          //console.log(e.target.value);
+          setText(e.target.value);
+      }}></input>
+      <button onClick={()=>setEdit(false)}>수정</button>
+        </div>
+    )
+  }
+  else
   return (
-    <div className="App">
-      <header className="App-header">
-        <div>{name}</div>
-      </header>
+    <div>{text}
+      <button onClick={() => setEdit(true)}>수정</button>
     </div>
-  );
+  )
 }
 
 export default App;

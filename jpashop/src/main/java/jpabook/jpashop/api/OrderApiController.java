c20e30815dde3b49c9c9629b22ff4b250c7cf888
DongHoonYu96 <dbehdgns71@naver.com>
2023-12-01 02:25:29 +0900

    feat : 1:다 엔티티를 dto로 반환 구현 v2.1

--- jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -1,10 +1,15 @@
 package jpabook.jpashop.api;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,4 +34,42 @@ public class OrderApiController {
         }
         return all;
     }
+
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<OrderDto> collect = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+        return collect;
+    }
+
+    @Data   //no properties 오류해결 : @Getter(@Data) 만들어주기
+    static class OrderDto {
+
+        private Long orderId;
+        private String name;
+        private LocalDateTime localDateTime;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItem> orderItems;
+
+        public OrderDto(Order order) {
+            this.orderId = order.getId();
+            this.name = order.getMember().getName();
+            this.localDateTime = order.getOrderDate();
+            this.orderStatus = order.getStatus();
+            this.address = order.getDelivery().getAddress();
+            order.getOrderItems().stream().forEach(o -> o.getItem().getName()); //LAZY 엔티티라서 null -> 강제초기화
+            this.orderItems = order.getOrderItems();
+            //단점 : 결국 외부에 엔티티 의존성이 높음, orderItem수정시 api 스펙이 다바뀜
+            //       Dto한번 감싸는것만으로 안됨
+            //해결 : orderItem을 orderItemDto로 바꿔야함
+        }
+    }
+
+
 }
+
+
+

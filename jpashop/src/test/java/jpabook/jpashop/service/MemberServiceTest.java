df7e39d7fb0339637b37355832310ffac1cc75cb
DongHoonYu96 <sbna154@gmail.com>
2025-04-01 16:06:31 +0900

    test: member service - update

--- jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -2,15 +2,19 @@ package jpabook.jpashop.service;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.repository.MemberRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.Assert.*;
 
+@ActiveProfiles("test")
 @RunWith(SpringRunner.class)    //JUnit 실행할때 스프링이랑 같이 실행해줘
 @SpringBootTest //Spring boot 띄운상태로 테스트 돌려줘. (없으면 Autowired 안됨)
 @Transactional
@@ -28,8 +32,7 @@ public class MemberServiceTest {
         //when
         Long savedId= memberService.join(member);
 
-        //then
-        assertEquals(member,memberRepository.findOne(savedId)); //멤버랑 찾은얘랑 같은가? ㅇㅇ 같은 영속성에서 pk가 같으면 같음.
+        assertThat(member).isEqualTo(memberRepository.findOne(savedId));
     }
 
     @Test(expected = IllegalStateException.class)
@@ -46,6 +49,21 @@ public class MemberServiceTest {
         memberService.join(member2);    //예외가 발생해야한다.
 
         //then
-        fail("예외가 발생해야 한다.");   //여기줄까지 오면안됨
+        Assertions.fail("예외가 발생해야 한다.");
+    }
+
+    @Test
+    public void 업데이트() {
+        //given
+        Member member = new Member();
+        member.setName("kim");
+        Long savedId = memberService.join(member);
+
+        //when
+        memberService.update(savedId, "lee");
+        Member findMember = memberService.findOne(savedId);
+
+        //then
+        assertThat(findMember.getName()).isEqualTo("lee");
     }
 }

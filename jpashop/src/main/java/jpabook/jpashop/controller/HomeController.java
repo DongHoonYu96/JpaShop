1232e526ada3eefcfac55586f7520e81a6bfce4f
DongHoonYu96 <sbna154@gmail.com>
2025-04-11 20:19:37 +0900

    feat: login member argument resolver 구현

--- jpashop/src/main/java/jpabook/jpashop/controller/HomeController.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/HomeController.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.controller;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.web.SessionConst;
+import jpabook.jpashop.web.argumentresolver.Login;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,12 +13,25 @@ import org.springframework.web.bind.annotation.SessionAttribute;
 @Slf4j
 public class HomeController {
 
-    @RequestMapping("/")
+//    @RequestMapping("/")
     public String home(
             @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember,
             Model model
             ) {
 
+        if (loginMember == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+    @RequestMapping("/")
+    public String homeArgumentResolver(
+            @Login Member loginMember,
+            Model model
+    ) {
         if (loginMember == null) {
             return "home";
         }

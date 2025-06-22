b33afb57b9bc77b1da477ddf1c213c1fb26005ba
DongHoonYu96 <sbna154@gmail.com>
2025-06-23 00:28:47 +0900

    chore: 필요없는 주석삭제

--- jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -13,15 +13,15 @@ import javax.validation.constraints.NotNull;
 
 import lombok.*;
 
-@Entity //Member 테이블 만들어줘.
+@Entity
 @Getter
 @Setter
 @NoArgsConstructor
 public class Member {
 
-    @Id //Long id를 키로 해줘.
-    @GeneratedValue //기본키 자동으로 생성해줘(1씩증가)
-    @Column(name = "member_id") //테이블 컬럼명 이걸로해줘. (기본값:변수이름)
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
 
     @NotNull
@@ -33,16 +33,11 @@ public class Member {
     @NotNull
     private String name;
 
-    @Embedded //내장타입임.
+    @Embedded
     private Address address;
 
-    @OneToMany(mappedBy = "member") // Member입장에서 Order는 일대다
-    /*
-    연관관계주인 : FK에 가까운 Order
-    거울쪽에 mappedBy를 적어준다.
-    나는 Order클래스의 member변수의 거울이다.
-     */
-    @JsonIgnore //api 요청시 반환안함!
+    @OneToMany(mappedBy = "member")
+    @JsonIgnore
     private List<Order> orders = new ArrayList<>();
 
     @Builder
